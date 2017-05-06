2a4b5dcc258939e553979d8dfb2916daed97eb34
Seongmuk Gang <liprin1129@gmail.com>
2017-05-07 00:41:53 +0900

    12.Lab: Feature Extraction

--- 3. Transfer Learning/feature_extraction.py
+++ 3. Transfer Learning/feature_extraction.py
@@ -19,7 +19,11 @@ fc7 = AlexNet(resized, feature_extract=True)
 # HINT: Look at the final layer definition in alexnet.py to get an idea of what this
 # should look like.
 shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
-probs = ...
+print(shape)
+fc8W = tf.Variable(tf.truncated_normal([4096, 43], stddev=1e-2))
+fc8b = tf.Variable(tf.zeros(nb_classes))
+logits = tf.nn.xw_plus_b(fc7, fc8W, fc8b)
+probs = tf.nn.softmax(logits)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
